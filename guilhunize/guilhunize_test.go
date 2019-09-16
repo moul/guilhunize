3e7cd954bcfc099c8b76da3a2343ad6a4ae79601
Manfred Touron <m@42.am>
2019-09-16 23:12:40 +0200

    chore: update example

--- guilhunize/guilhunize_test.go
+++ guilhunize/guilhunize_test.go
@@ -3,6 +3,7 @@ package guilhunize
 import "fmt"
 
 func ExampleGuilhunize() {
-	fmt.Println(Guilhunize("salut, j'aime bien faire des interfaces dans les refactors, surtout lorsqu'il faut faire du chiffrement ou du déchiffrement."))
+	input := "salut, j'aime bien faire des interfaces dans les refactors, surtout lorsqu'il faut faire du chiffrement ou du déchiffrement."
+	fmt.Println(Guilhunize(input))
 	// Output: salut, j'aime bien faire des i-love-interfaces dans les refractos, surtout lorsqu'il faut faire du cryptage ou du désenchiffrement.
 }
